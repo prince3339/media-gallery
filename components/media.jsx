c705299509311b33a101ad997492f9d9c7917461
Sabbir <prince3339@ymail.com>
2021-08-14 19:15:38 +0600

    Search Component implemented, pagination fix

--- components/media.jsx
+++ components/media.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import axios from 'axios';
-import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Box,
     Card,
 		Radio,
+		TextField,
 		RadioGroup,
     CardActionArea,
     CardContent,
@@ -17,6 +17,7 @@ import {
 		FormControlLabel
 } from '@material-ui/core';
 import queryString from 'querystring';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const useStyles = makeStyles({
   root: {
@@ -31,16 +32,24 @@ export default function MediaCard() {
 	const [isLoading, setLoading] = useState(true);
 	const [mediaType, setMediaType] = useState(null);
 	const [searchText, setSearchTex] = useState(null);
-	const [medias, setMedias] = useState([]);
+	const [metadata, setMetadata] = useState(null);
+	const [renderedData, setRenderedData] = useState([]);
+	const [offset, setOffset] = useState(0);
 
 	const classes = useStyles();
 
 	const handleChange = (event) => {
+		setOffset(0);
     setMediaType(event.target.value);
+		setRenderedData([]);
   };
 
+	const loadMore = () => {
+		setOffset(offset + 40);
+	}
+
 	useEffect(() => {
-		async function fetchData() {
+		const fetchData = async () => {
 			const authCredentials = process.env.NEXT_PUBLIC_AUTH_CREDENTIAL;
 			const encodedCredentials = Buffer.from(authCredentials).toString('base64');
 			const headers = {
@@ -49,7 +58,7 @@ export default function MediaCard() {
 			}
 			try {
 				const query = {
-					offset: 0,
+					offset,
 					limit: 40,
 					searchText: searchText || null,
 					...(mediaType && {type: mediaType})
@@ -57,22 +66,33 @@ export default function MediaCard() {
 				const mediaData = await axios.get(`${process.env.NEXT_PUBLIC_MEDIA_BASE_URL}/api/v1/medias?${queryString.stringify(query)}`, {
 					headers
 				});
-
-				setMedias(mediaData.data);
+	
+				setMetadata(mediaData.data.metadata);
+				setRenderedData((renderedData) => {
+					return [...renderedData, ...mediaData.data.items];
+				});
 				setLoading(false)
 			} catch (err) {
 				console.log('Something went wrong', err);
 			}
-		}
+		};
+	
 		fetchData();
-	}, [mediaType, searchText]);
+	}, [searchText, mediaType, offset]);
 
 	if (isLoading) {
 		return 'Loading...';
 	}
 
+	if (metadata?.pagination?.totalCount === 0 && !isLoading) {
+		return 'No Data found!';
+	}
+
+	const { pagination: { totalCount } } = metadata;
+
   return (
     <Box padding={4}>
+			
 			<FormControl component="fieldset">
 				<FormLabel component="legend">Media Type</FormLabel>
 				<RadioGroup aria-label="gender" name="gender1" value={mediaType} onChange={handleChange}>
@@ -80,32 +100,51 @@ export default function MediaCard() {
 					<FormControlLabel value="VIDEO" control={<Radio />} label="Video" />
 				</RadioGroup>
 			</FormControl>
-      <Box
-				paddingTop={3}
-				display="flex"
-				justifyContent="flex-start"
-				alignItems="start"
-				gridGap={20}
-				flexWrap="wrap"
-			>
-				{medias.items?.map((media) => {
-					return (
-						<Card key={media.id} className={classes.root}>
-							<CardActionArea>
-									{media.type === 'IMAGE' ? 
-										<img src={media.mediaUrl} alt={media.title} width="100%" height="200" style={{objectFit: 'cover'}} />
-										: <video src={media.mediaUrl}>Not supported</video>
-									}
-									<CardContent>
-									<Typography gutterBottom variant="h5" component="h2">
-											{media.title}
-									</Typography>
-									</CardContent>
-							</CardActionArea>
-						</Card>
-					)
-				})}
-			</Box>
+			<Box paddingTop={2}>
+				<TextField id="outlined-search" label="Search by text" type="search" variant="outlined" onChange={(e) => {
+					setOffset(0);
+					setSearchTex(e.target.value);
+					setRenderedData([]);
+				}} />
+			</Box>	
+			<InfiniteScroll
+					dataLength={totalCount} //This is important field to render the next data
+					next={() => loadMore()}
+					hasMore={true}
+					loader={<h4>Loading...</h4>}
+					endMessage={
+						<p style={{ textAlign: 'center' }}>
+							<b>Yay! You have seen it all</b>
+						</p>
+					}
+				>
+					<Box
+						paddingTop={3}
+						display="flex"
+						justifyContent="flex-start"
+						alignItems="start"
+						gridGap={20}
+						flexWrap="wrap"
+					>
+						{renderedData.map((media) => {
+							return (
+								<Card key={media.id} className={classes.root}>
+									<CardActionArea>
+											{media.type === 'IMAGE' ? 
+												<img src={media.mediaUrl} alt={media.title} width="100%" height="200" style={{objectFit: 'cover'}} />
+												: <video src={media.mediaUrl}>Not supported</video>
+											}
+											<CardContent>
+											<Typography gutterBottom variant="h5" component="h2">
+													{media.title}
+											</Typography>
+											</CardContent>
+									</CardActionArea>
+								</Card>
+							)
+						})}
+					</Box>	
+				</InfiniteScroll>
     </Box>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import MediaCard from '../components/media';
@@ -16,19 +15,6 @@ export default function Home() {
       <main className={styles.main}>
         <MediaCard />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
